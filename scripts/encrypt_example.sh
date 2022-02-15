40c0b05a82a393bfcba81b6efaca466977c856ce
Łukasz Gryglicki <lgryglicki@cncf.io>
2022-02-15 05:53:38 +0000

    Unblocked encryption keys
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- scripts/encrypt_example.sh
+++ scripts/encrypt_example.sh
@@ -2,7 +2,7 @@
 export AWS_REGION="`cat ./secrets/AWS_REGION.dev.secret`"
 export AWS_ACCESS_KEY_ID="`cat ./secrets/AWS_ACCESS_KEY_ID.dev.secret`"
 export AWS_SECRET_ACCESS_KEY="`cat ./secrets/AWS_SECRET_ACCESS_KEY.dev.secret`"
-export ENCRYPTION_KEY=encryption-key-name
-export ENCRYPTION_BYTES=encryption-bytes
+export ENCRYPTION_KEY="`cat ./secrets/ENCRYPTION_KEY.dev.secret`"
+export ENCRYPTION_BYTES="`cat ./secrets/ENCRYPTION_BYTES.dev.secret`"
 ./encrypt "`cat /etc/github/oauths`" > ./secrets/tokens.encrypted.secret || exit 1
 ./github --github-es-url="`cat ./secrets/ES_URL.prod.secret`" --github-debug=1 --github-categories='pull_request' --github-tokens="`cat ./secrets/tokens.encrypted.secret`" --github-date-from=2020-10-25 --github-date-to=2020-10-27 --github-stream='' --github-org=magma --github-repo=magma-website
