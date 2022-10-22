144500d1995673aa4a00051baec4e40c7253aea8
Ayman <enkhalifapro@gmail.com>
2022-10-22 20:03:25 +0200

    check traces
    Signed-off-by: Ayman <enkhalifapro@gmail.com>

--- cmd/github/github.go
+++ cmd/github/github.go
@@ -9138,3 +9138,22 @@ func (j *DSGitHub) preventUpdateIssueDuplication(v []interface{}, event string) 
 	}
 	return updates, cacheData, nil
 }
+
+// NoopSpanContext is an implementation of ddtrace.SpanContext that is a no-op.
+type NoopSpanContext struct {
+	spanID  uint64
+	traceID uint64
+}
+
+func NewSpanContext(traceID uint64, spanID uint64) NoopSpanContext {
+	return NoopSpanContext{traceID: traceID, spanID: spanID}
+}
+
+// SpanID implements ddtrace.SpanContext.
+func (n NoopSpanContext) SpanID() uint64 { return n.spanID }
+
+// TraceID implements ddtrace.SpanContext.
+func (n NoopSpanContext) TraceID() uint64 { return n.traceID }
+
+// ForeachBaggageItem implements ddtrace.SpanContext.
+func (n NoopSpanContext) ForeachBaggageItem(handler func(k, v string) bool) {}
